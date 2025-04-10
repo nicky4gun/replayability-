7e56e7e9d1f070c6f80a933047858e3d9ee054c8
TheDodoTaco <markoliver@godmail.dk>
2025-04-10 13:31:04 +0200

    Deathcount maybe (Nicklas skal lige adde til sin egen)

---
+++ Assets/skrips/DeathCount.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeathCounter : MonoBehaviour
+{
+    public static DeathCounter Instance;
+    public int deathCount = 0;
+    public Text deathText; // Assign in the inspector if you want UI
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Reset deaths only for scenes that start with "Level"
+        if (scene.name.StartsWith("Level"))
+        {
+            ResetDeaths();
+        }
+    }
+
+    public void AddDeath()
+    {
+        deathCount++;
+
+        Debug.Log("Deaths: " + deathCount);
+
+        if (deathText != null)
+        {
+            deathText.text = "Deaths: " + deathCount.ToString();
+        }
+    }
+
+    public void ResetDeaths()
+    {
+        deathCount = 0;
+
+        if (deathText != null)
+        {
+            deathText.text = "Deaths: " + deathCount.ToString();
+        }
+    }
+}
