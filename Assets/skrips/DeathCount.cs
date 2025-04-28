558f64c0c71cbe50dcf4806edd25bbccdf84ab28
nicky4gun <nicklas.stagesorensen@outlook.dk>
2025-04-28 15:35:45 +0200

    now fix

--- Assets/skrips/DeathCount.cs
+++ Assets/skrips/DeathCount.cs
@@ -1,37 +1,57 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class DeathCounter : MonoBehaviour
 {
     public static DeathCounter Instance;
-    public int deathCount = 0;
-    public TMP_Text deathText;
+    public static int deathCount = 0;
+    public TMP_Text deathText; // UI Text to display death count
+
+    public TMP_Text timer;
+    public static int time = 0;
 
     void Awake()
     {
+        // Ensure only one instance of DeathCounter exists
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject); // Prevent this object from being destroyed on scene load
         }
         else
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // If another instance exists, destroy this one
         }
     }
 
-    void Update()
-    {
-        // If deathText is missing, try to find it again
+    void Start()
+    {/*
+        // Try to find the DeathText if it's not assigned
         if (deathText == null)
         {
-            GameObject foundText = GameObject.Find("DeathText"); // <-- your UI object name!
+            GameObject foundText = GameObject.Find("Death Text");
             if (foundText != null)
             {
                 deathText = foundText.GetComponent<TMP_Text>();
-                UpdateUI();
             }
         }
+        if (timer == null)
+        {
+            GameObject foundTextTimer = GameObject.Find("TimerTxt");
+            if (foundTextTimer != null)
+            {
+                timer = foundTextTimer.GetComponent<TMP_Text>();
+            }
+        }*/
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = (int)Time.time;
+        timer.text = time.ToString();
     }
 
     public void AddDeath()
